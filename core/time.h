1abd479bb59c31eacaf4c059cb3d75dfddb63911
José Pazos Pérez <jose.pazos.perez@rai.usc.es>
2021-05-27 22:31:56 +0200

    [0.1.8] Timer with Game Speed ⏰

--- core/time.h
+++ core/time.h
@@ -9,11 +9,19 @@
 
 namespace Verse
 {
+    struct Timer {
+        float current = 0;
+        ui32 previous;
+        ui32 duration;
+        Timer() = default;
+        Timer(ui32 p_d, ui32 p_p) : duration(p_d), previous(p_p) {};
+    };
+
     struct Time {
         static ui32 current;
         static ui32 previous;
         static ui32 delta;
-        static std::map<ui32, bool*> timers;
+        static std::map<ui32, Timer> timers;
     };
 
     ui32 time();
@@ -22,6 +30,6 @@ namespace Verse
     float time_precise_difference(ui64 t1, ui64 t2);
     
     ui32 setTimer(ui32 ms);
-    bool checkTimer(ui32 timer);
+    bool checkTimer(ui32 timer, float game_speed = 1.0f);
     void stopTimer(ui32 timer);
 }
