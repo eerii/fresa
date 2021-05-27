26709696c4c50d2e81511c46ab0221eaa20b2b77
José Pazos Pérez <jose.pazos.perez@rai.usc.es>
2021-05-27 22:31:56 +0200

    [0.1.8] Timer with Game Speed ⏰

--- core/time.cpp
+++ core/time.cpp
@@ -4,12 +4,18 @@
 
 #include "time.h"
 
+#include "log.h"
+
 using namespace Verse;
+
+namespace {
+    ui32 last_timer_id = 0;
+}
 
 ui32 Time::current = 0;
 ui32 Time::previous = 0;
 ui32 Time::delta = 0;
-std::map<ui32, bool*> Time::timers = {};
+std::map<ui32, Timer> Time::timers = {};
 
 ui32 Verse::time() {
     return SDL_GetTicks();
@@ -28,37 +34,24 @@ float Verse::time_precise_difference(ui64 t1, ui64 t2) {
 }
 
 ui32 Verse::setTimer(ui32 ms) {
-    auto timer_function = [](ui32 interval, void* param) {
-        SDL_Event event;
-        SDL_UserEvent user_event;
-        
-        user_event.type = SDL_USEREVENT;
-        user_event.code = 0;
-        user_event.data1 = param;
-        user_event.data2 = NULL;
-        
-        event.type = SDL_USEREVENT;
-        event.user = user_event;
-        
-        SDL_PushEvent(&event);
-        return(interval);
-    };
-    ui32 (*timer_function_ptr) (ui32, void*) = timer_function;
+    ui32 timer_id = last_timer_id + 1;
+    while (Time::timers.find(timer_id) != Time::timers.end())
+        timer_id += 1;
+    last_timer_id = timer_id;
     
-    bool* done = new bool(false);
-    void* done_ptr = reinterpret_cast<void*>(done);
-    
-    ui32 timer = SDL_AddTimer(ms, timer_function_ptr, done_ptr);
-    Time::timers[timer] = done;
-    
-    return timer;
+    Time::timers[timer_id] = Timer(ms, time());
+    return timer_id;
 }
 
-bool Verse::checkTimer(ui32 timer) {
+bool Verse::checkTimer(ui32 timer, float game_speed) {
     if (Time::timers.find(timer) == Time::timers.end())
         return false;
     
-    bool done = *Time::timers[timer];
+    ui32 curr = time();
+    Time::timers[timer].current += (float)(curr - Time::timers[timer].previous) * game_speed;
+    Time::timers[timer].previous = curr;
+    
+    bool done = Time::timers[timer].current >= Time::timers[timer].duration;
     if (done)
         Time::timers.erase(timer);
     
@@ -66,6 +59,5 @@ bool Verse::checkTimer(ui32 timer) {
 }
 
 void Verse::stopTimer(ui32 timer) {
-    SDL_RemoveTimer(timer);
     Time::timers.erase(timer);
 }
