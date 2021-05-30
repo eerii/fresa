eb7fcd392b7ecdfb05a0547724e8f898c31b0f78
José Pazos Pérez <jose.pazos.perez@rai.usc.es>
2021-05-30 12:26:50 +0200

    [0.1.9] Error Handling ⚠️

--- graphics/r_opengl.h
+++ graphics/r_opengl.h
@@ -23,3 +23,35 @@
     #include <SDL2/SDL_opengl_glext.h>
 
 #endif //!__APPLE__
+
+#include <iostream>
+#include <string>
+
+#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+#define glCheckError() glCheckError_(__FILENAME__, __LINE__)
+
+#define GL_STACK_OVERFLOW 0x0503
+#define GL_STACK_UNDERFLOW 0x0504
+
+namespace Verse
+{
+
+    [[maybe_unused]] static void glCheckError_(std::string file, int line) {
+        GLenum e_code;
+        while ((e_code = glGetError()) != GL_NO_ERROR) {
+            std::string e_message;
+            switch (e_code)
+            {
+                case GL_INVALID_ENUM:                  e_message = "INVALID_ENUM"; break;
+                case GL_INVALID_VALUE:                 e_message = "INVALID_VALUE"; break;
+                case GL_INVALID_OPERATION:             e_message = "INVALID_OPERATION"; break;
+                case GL_STACK_OVERFLOW:                e_message = "STACK_OVERFLOW"; break;
+                case GL_STACK_UNDERFLOW:               e_message = "STACK_UNDERFLOW"; break;
+                case GL_OUT_OF_MEMORY:                 e_message = "OUT_OF_MEMORY"; break;
+                case GL_INVALID_FRAMEBUFFER_OPERATION: e_message = "INVALID_FRAMEBUFFER_OPERATION"; break;
+            }
+            std::cout << "[GL_ERROR]: " << e_message << " | " << file << " (" << line << ")" << std::endl;
+        }
+    }
+
+}
