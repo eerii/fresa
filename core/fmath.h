dc90e14c26d94101e5ce975d665662e6efba40a2
José Pazos Pérez <jose.pazos.perez@rai.usc.es>
2021-06-27 00:55:36 +0200

    [0.1.14] Camera is Stable 🥳

--- core/fmath.h
+++ core/fmath.h
@@ -8,7 +8,7 @@
 
 namespace Verse::Math
 {
-    void perlinNoise(Vec2 size, Vec2 offset, float freq, int levels, ui8* noise_data, bool reset = false);
-
     bool checkAABB(Rect2 &a, Rect2 &b);
+    void perlinNoise(Vec2 size, Vec2 offset, float freq, int levels, ui8* noise_data, bool reset = false);
+    float smoothDamp(float current, float target, float &current_vel, float time, float max_speed, float delta_time);
 }
