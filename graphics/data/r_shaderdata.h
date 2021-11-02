011c7f1b6ed23d2efb644da16d79085ea33f8e10
José Pazos Pérez <jose.pazos.perez@rai.usc.es>
2021-11-02 22:32:59 +0100

    [0.2.5] Proper Vulkan API Phase IV 🛩

--- graphics/data/r_shaderdata.h
+++ graphics/data/r_shaderdata.h
@@ -7,17 +7,47 @@
 #include "dtypes.h"
 #include <map>
 
+#ifdef USE_VULKAN
+#include "spirv_glsl.hpp" //SPIRV-cross for reflection
+#endif
+
 namespace Verse::Graphics
 {
+    #ifdef USE_VULKAN
+    using ShaderCompiler = spirv_cross::CompilerGLSL;
+    using ShaderResources = spirv_cross::ShaderResources;
+
+    struct ShaderStages {
+        std::optional<VkShaderModule> vert;
+        std::optional<VkShaderModule> frag;
+        std::optional<VkShaderModule> compute;
+        std::optional<VkShaderModule> geometry;
+    };
+
+    struct ShaderCode {
+        std::optional<std::vector<char>> vert;
+        std::optional<std::vector<char>> frag;
+        std::optional<std::vector<char>> compute;
+        std::optional<std::vector<char>> geometry;
+    };
+    #endif
+
+    struct ShaderLocations {
+        std::optional<str> vert;
+        std::optional<str> frag;
+        std::optional<str> compute;
+        std::optional<str> geometry;
+    };
+
     struct ShaderData {
-        str vertex_file;
-        str frag_file;
+        ShaderLocations locations;
         
     #if defined USE_OPENGL
         ui8 pid;
         std::map<str, ui8> locations;
     #elif defined USE_VULKAN
-        
+        ShaderCode code;
+        ShaderStages stages;
     #endif
     };
 }
