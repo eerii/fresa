5b376828bb5bd6b843904d5a4a775d9a4c487a11
José Pazos Pérez <jose.pazos.perez@rai.usc.es>
2021-11-27 18:49:37 +0100

    [0.2.5] Functional Draw API 🦄

--- graphics/data/r_renderdata.h
+++ graphics/data/r_renderdata.h
@@ -12,15 +12,7 @@
 
 namespace Verse::Graphics
 {
-    #if defined USE_VULKAN
-        using GraphicsAPI = Vulkan;
-    #elif defined USE_OPENGL
-        using GraphicsAPI = OpenGL;
-    #endif
-
     struct RenderData {
-        GraphicsAPI api;
-        
         Vec2<> resolution;
         ui16 scale;
         bool vsync;
