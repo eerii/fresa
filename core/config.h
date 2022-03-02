333185c29335b5ebe83df23f6b2965c4a19f6308
José Pazos Pérez <jose.pazos.perez@rai.usc.es>
2022-03-02 18:27:24 +0100

    [0.3.7] Config Customizability Fix 🦦

--- core/config.h
+++ core/config.h
@@ -8,10 +8,10 @@
 
 namespace Fresa::Config
 {
-    inline const str name = "Proxecto Fresa";
-    inline const ui8 version[3] = {0, 3, 7};
-    inline const Vec2<ui32> window_size = {1024, 720};
-    inline const Vec2<ui32> resolution = {256, 180};
+    inline str name = "Proxecto Fresa";
+    inline ui8 version[3] = {0, 3, 7};
+    inline Vec2<ui32> window_size = {1024, 720};
+    inline Vec2<ui32> resolution = {256, 180};
     
     inline const float timestep = 10.0f;
     inline float game_speed = 1.0f;
