9f20f91d60dcba4967286b4dec426339f84ff35f
josekoalas <jose.pazos.perez@rai.usc.es>
2022-06-13 21:57:42 +0200

    [0.4.0] Test Suite III 🌸

--- tools/log.h
+++ tools/log.h
@@ -6,28 +6,30 @@
 #include <iostream>
 #include "fresa_types.h"
 #include "fmt/core.h"
-
-//* log levels
-//      1 << 0 - errors
-//      1 << 1 - warnings
-//      1 << 2 - info
-//      1 << 3 - graphics
-//      1 << 4 - test errors
-//      1 << 5 - test info and results
-//      1 << 6 - debug
-
-#ifndef LOG_LEVEL
-#define LOG_LEVEL 0b0110111
-#endif
+#include "fmt/color.h"
 
 namespace fresa
 {
+    //* log level
+    enum log_levels {
+        LOG_ERROR = 1 << 0,
+        LOG_WARNING = 1 << 1,
+        LOG_INFO = 1 << 2,
+        LOG_GRAPHICS = 1 << 3,
+        LOG_TEST = 1 << 4,
+        LOG_DEBUG = 1 << 5
+    };
+
+    #ifndef LOG_LEVEL
+    #define LOG_LEVEL 0b111111
+    #endif
+
     namespace detail
     {
-        template<str_literal name, std::size_t level, typename ...T>
+        template<str_literal name, std::size_t level, fmt::color color = fmt::color::white, typename ...T>
         void log(const T& ...t) {
-            if constexpr(LOG_LEVEL & level) {
-                fmt::print("[{}]: ", name.value);
+            if constexpr((LOG_LEVEL & level) == level) {
+                fmt::print(fg(color), "[{}]: ", name.value);
                 fmt::print(t...);
                 fmt::print("\n");
             }
@@ -37,27 +39,18 @@ namespace fresa
     namespace log
     {
         template <typename ...T>
-        void error(const T& ...t) { detail::log<"ERROR", 1 << 0>(t...); }
+        void error(const T& ...t) { detail::log<"ERROR", LOG_ERROR, fmt::color::red>(t...); }
 
         template <typename ...T>
-        void warn(const T& ...t) { detail::log<"WARN", 1 << 1>(t...); }
+        void warn(const T& ...t) { detail::log<"WARN", LOG_WARNING, fmt::color::gold>(t...); }
 
         template <typename ...T>
-        void info(const T& ...t) { detail::log<"INFO", 1 << 2>(t...); }
+        void info(const T& ...t) { detail::log<"INFO", LOG_INFO, fmt::color::cornflower_blue>(t...); }
 
         template <typename ...T>
-        void graphics(const T& ...t) { detail::log<"GRAPHICS", 1 << 3>(t...); }
+        void graphics(const T& ...t) { detail::log<"GRAPHICS", LOG_GRAPHICS, fmt::color::dark_turquoise>(t...); }
 
         template <typename ...T>
-        void test_error(const T& ...t) { detail::log<"TEST FAILED", 1 << 4>(t...); }
-
-        template <typename ...T>
-        void test_passed(const T& ...t) { detail::log<"TEST PASSED", 1 << 5>(t...); }
-
-        template <typename ...T>
-        void test(const T& ...t) { detail::log<"TEST", 1 << 5>(t...); }
-
-        template <typename ...T>
-        void debug(const T& ...t) { detail::log<"DEBUG", 1 << 6>(t...); }
+        void debug(const T& ...t) { detail::log<"DEBUG", LOG_DEBUG, fmt::color::lime_green>(t...); }
     }
 }
