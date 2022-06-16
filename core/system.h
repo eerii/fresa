c62b1cc757297506d5d469d76d1a15e440705306
josekoalas <jose.pazos.perez@rai.usc.es>
2022-06-16 13:46:21 +0200

    [0.4.0] Subsystem Queues 🍁

--- core/system.h
+++ core/system.h
@@ -1,71 +1,87 @@
 //* system
-//      ...
+//      manages the engine's subsystems, allowing for creation, iteration with priority, and destruction in order
 
 #include "fresa_types.h"
 #include "log.h"
 #include "unit_test.h"
+#include <queue>
 #include <stack>
 
-namespace fresa
+namespace fresa::system
 {
     namespace detail
     {
         //* system concept, requires an initialacion and stop function
         template <typename T>
-        concept TSystem = requires(T system) {
+        concept TSystem = requires(T s) {
             T::init();
             T::stop();
         };
 
         //* system with simulation update
         template <typename T>
-        concept TSystemUpdate = TSystem<T> and requires(T system) {
+        concept TSystemUpdate = TSystem<T> and requires(T s) {
             T::update();
         };
 
-        //* handle for a function with a name attached to it
-        struct FunctionHandle {
+        //* objects for the system update and stop lists
+        //      a lower number means higher priority, so a system with priority 2 comes before a system with priority 4
+        struct SystemUpdateObject {
+            ui8 priority;
+            str_view name;
+            void (*f)();
+        };
+        struct SystemStopObject {
             str_view name;
             void (*f) ();
         };
     }
 
+    //* system priorities
+    enum SystemPriorities {
+        SYSTEM_PRIORITY_FIRST = 0,
+        SYSTEM_PRIORITY_DEFAULT = 100,
+        SYSTEM_PRIORITY_LAST = 200,
+    };
+
     //* system manager
     inline struct SystemManager {
-        std::vector<detail::FunctionHandle> update;
-        std::stack<detail::FunctionHandle> stop;
-    } systems;
+        std::priority_queue<detail::SystemUpdateObject, std::deque<detail::SystemUpdateObject>, 
+                            decltype([](auto a, auto b){ return a.priority >= b.priority; })> update;
+        std::stack<detail::SystemStopObject> stop;
+    } manager;
 
     //* register and initialize system
-    void registerSystem(detail::TSystem auto system) {
-        constexpr auto name = type_name<decltype(system)>();
+    void add(detail::TSystem auto s, ui8 priority = SYSTEM_PRIORITY_DEFAULT) {
+        constexpr auto name = type_name<decltype(s)>();
         log::debug("registering system '{}'", name);
 
         //* initialize system
-        system.init();
+        s.init();
         
         //* add to update list
-        if constexpr (detail::TSystemUpdate<decltype(system)>)
-            systems.update.push_back({name, system.update});
+        if constexpr (detail::TSystemUpdate<decltype(s)>)
+            manager.update.push({priority, name, s.update});
 
         //* add to destructor list
-        systems.stop.push({name, system.stop});
+        manager.stop.push({name, s.stop});
     }
 
     //* clean all systems in inverse order when program stops
-    void stopSystems() {
-        while (not systems.stop.empty()) {
-            log::debug("stopping system '{}'", systems.stop.top().name);
-            systems.stop.top().f();
-            systems.stop.pop();
+    void stop() {
+        while (not manager.stop.empty()) {
+            log::debug("stopping system '{}'", manager.stop.top().name);
+            manager.stop.top().f();
+            manager.stop.pop();
         }
     }
 }
 
+//* unit tests
 namespace test
 {
     using namespace fresa;
-    inline TestSuite system_test("system", []{
+    inline TestSuite system_test("systems", []{
         struct SystemA {
             static void init() {};
             static void update() {};
@@ -74,24 +90,39 @@ namespace test
 
         struct SystemB {
             static void init() {};
+            static void update() {};
             static void stop() {};
         };
 
-        "before adding systems"_test = []{ return expect(systems.update.size() == 0 and systems.stop.size() == 0); };
-        "adding system A"_test = []{
-            registerSystem(SystemA{});
-            return expect(systems.update.size() == 1 and systems.stop.size() == 1);
+        struct SystemC {
+            static void init() {};
+            static void stop() {};
         };
-        "adding system B"_test = []{
-            registerSystem(SystemB{});
-            return expect(systems.update.size() == 1 and systems.stop.size() == 2);
+
+        "before adding systems"_test = []{
+            return expect(system::manager.update.size() == 0 and system::manager.stop.size() == 0);
+        };
+        "adding system A with default update priority (100)"_test = []{
+            system::add(SystemA{});
+            return expect(system::manager.update.size() == 1 and system::manager.stop.size() == 1);
+        };
+        "adding system B with higher update priority (0)"_test = []{
+            system::add(SystemB{}, system::SYSTEM_PRIORITY_FIRST);
+            return expect(system::manager.update.size() == 2 and system::manager.stop.size() == 2);
+        };
+        "B has higher update priority than A"_test = []{
+            return expect(system::manager.update.top().name == "SystemB");
+        };
+        "adding system C"_test = []{
+            system::add(SystemC{});
+            return expect(system::manager.update.size() == 2 and system::manager.stop.size() == 3);
         };
         "check stop order"_test = []{
-            return expect(systems.stop.top().name == "SystemB");
+            return expect(system::manager.stop.top().name == "SystemC");
         };
         "stop systems"_test = []{
-            stopSystems();
-            return expect(systems.stop.size() == 0);
+            system::stop();
+            return expect(system::manager.stop.size() == 0);
         };
     });
 }
